708ea9aa6aab1281327d4150708615dfa88a1045
NANAA <nathan.lebon@epitech.eu>
2020-06-12 10:04:22 -0400

    server done

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const cors = require('cors');
+const conversion = require('./convert')
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send("Welcome to the best currency converter server");
+});
+
+
+app.get('/convert', (req, res) => {
+  conversion.handleConvertion(req, res);
+
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
---
+++ convert.js
@@ -0,0 +1,46 @@
+const request = require('request');
+
+
+const handleConvertion = (req, res) => {
+  const {body} = req;
+
+  if (!isBodyValid(body)) {
+    res.status(400).send("Missing Parameters");
+    return;
+  }
+  convertCurrency(body, res);
+
+};
+
+const convertCurrency = (body, res) => {
+  const url = `https://min-api.cryptocompare.com/data/price?fsym=${body.base_currency}&tsyms=${body.quote_currency}`;
+  request(url, (error, response, responseBody) => {
+    if (!error && response.statusCode === 200) {
+      responseBody = JSON.parse(responseBody);
+      console.log(responseBody)
+      const rate = responseBody[body.quote_currency];
+      const value = Number(body.value);
+      if (isNaN(value)) {
+        res.status(400).send("Invalid Value Parameter");
+        return;
+      }
+      const JSONresponse = {"converted_value" : rate * Number(body.value)};
+      res.send(JSONresponse)
+    }
+    else {
+      res.status(400).send("Invalid Parameters");
+    }
+  })
+};
+
+const isBodyValid = (body) => {
+  return (body.hasOwnProperty("base_currency" ) && body.hasOwnProperty("value") &&
+    body.hasOwnProperty("quote_currency"))
+};
+
+
+
+
+module.exports = {
+  handleConvertion
+};
